67ef218069536c0a28bf507ea23b8a5b22db61f3
Pranav Sojan <144614863+pranavsojan2026@users.noreply.github.com>
2024-02-19 11:37:13 +0530

    Add files via upload
    This Python script utilizes the PyDictionary library to look up the meaning of words entered by the user. Upon running the script, the user is prompted to input a word. The script then retrieves the meaning of the word using PyDictionary and displays it.

---
+++ word_dictionary.py
@@ -0,0 +1,10 @@
+from PyDictionary import PyDictionary
+
+dictionary = PyDictionary()
+
+while True:
+    word = input("Enter the word (press 1 to exit): ")
+    if word == "1":
+        break
+
+    print(dictionary.meaning(word))
